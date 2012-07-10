e42888a810c2db112bdb00d8825774f71215b6df
Nathan Ostgard <noonat@phuce.com>
2012-07-10 08:33:22 -0500

    Cleaned up HtmlFormatter

--- lib/pecs.php
+++ lib/pecs.php
@@ -52,7 +52,7 @@ class Runner {
         $this->suite = null;
         $this->suites = array();
     }
-    
+
     function describe($description, $func) {
         $suite = new Suite($description, $func, $this->suite);
         $this->suites[] = $suite;
@@ -67,7 +67,7 @@ class Runner {
             $this->hooks[$hook] = array();
         $this->hooks[$hook][] = $func;
     }
-    
+
     function it($description, $func) {
         $spec = new Spec($description, $func, $this->suite);
         $this->specs[] = $spec;
@@ -123,13 +123,13 @@ class Suite {
             $this->parent = null;
         }
     }
-    
+
     function hook($hook, $func) {
         if (!isset($this->hooks[$hook]))
             $this->hooks[$hook] = array();
         $this->hooks[$hook][] = $func;
     }
-    
+
     function parents() {
         $parents = array();
         for ($parent=$this->parent; $parent; $parent=$parent->parent) {
@@ -137,7 +137,7 @@ class Suite {
         }
         return $parents;
     }
-    
+
     function push($child) {
         $child instanceof Spec ? $this->specs[]=$child : $this->suites[]=$child;
     }
@@ -153,37 +153,37 @@ class Suite {
             }
         }
         return $scope;
-    }   
+    }
 }
 
 /// An actual test case.
 class Spec extends Suite {
     public $assertions = 0;
     public $failures = array();
-    
+
     function __construct($description=null, $func=null, $parent=null) {
         parent::__construct($description, $func, $parent);
         $this->description = $description;
     }
-    
+
     function expect($actualValue) {
         return new Expect($actualValue, $this);
     }
-    
+
     function fail($failure) {
         if (is_string($failure))
             $failure = new \Exception($failure);
         $this->failures[] = $failure;
     }
-    
+
     function failed() {
         return !$this->passed();
     }
-    
+
     function passed() {
         return empty($this->failures);
     }
-    
+
     function run($scope=array()) {
         $func = $this->func;
         try {
@@ -203,7 +203,7 @@ class Expect {
         $this->actual = $actual;
         $this->spec = $spec;
     }
-    
+
     function __call($method, $args) {
         $expectedResult = true;
         if (preg_match('/^(and_)?(not_)?(to_)?_*(.+)$/', $method, $matches)) {
@@ -217,7 +217,7 @@ class Expect {
         $this->_assert($method, $args, $expectedResult);
         return $this;
     }
-    
+
     function _assert($method, $args, $expectedResult) {
         $this->spec->assertions += 1;
         $values = (array)call_user_func_array(array($this, $method), $args);
@@ -225,7 +225,7 @@ class Expect {
         if ($result != $expectedResult)
             $this->_fail($method, $args, $result, $expectedResult, $values);
     }
-    
+
     function _fail($method, $args, $result, $expectedResult, $values) {
         if (empty($values)) {
             $format = 'expected %s to '.str_replace('_', ' ', $method);
@@ -245,7 +245,7 @@ class Expect {
             $message = $format;
         $this->spec->fail(new \Exception($message));
     }
-    
+
     static function _export($var) {
         if (is_array($var)) {
             $pairs = array();
@@ -265,7 +265,7 @@ class Expect {
             return var_export($var, true);
         }
     }
-    
+
     /// Matcher aliases
     public $_aliases = array(
         'be_an' => 'be_a',
@@ -274,65 +274,65 @@ class Expect {
         'have_count_within' => 'have_length_within',
         'throw' => 'throw_error',
     );
-    
+
     /// New matchers can be defined by adding a function below.
-    
+
     function be($expected) {
         return $this->actual === $expected;
     }
-    
+
     function be_greater_than($expected) {
         return $this->actual > $expected;
     }
-    
+
     function be_less_than($expected) {
         return $this->actual < $expected;
     }
-    
+
     function be_at_least($expected) {
         return $this->actual >= $expected;
     }
-    
+
     function be_at_most($expected) {
         return $this->actual <= $expected;
     }
-    
+
     function be_within($min, $max) {
         return array(
             $this->actual >= $min && $this->actual <= $max,
             'expected %s to be within %s and %s', $this->actual, $min, $max);
     }
-    
+
     function be_a($expected) {
         $class = get_class($this->actual);
         return array(
             $class === $expected,
             "expected $class to be class $expected");
     }
-    
+
     function be_an_instance_of($expected) {
         $class = get_class($this->actual);
         return array(
             $this->actual instanceof $expected,
             "expected $class to be an instance of $expected");
     }
-    
+
     function be_null() {
         return is_null($this->actual);
     }
-    
+
     function be_true() {
         return $this->actual === true;
     }
-    
+
     function be_false() {
         return $this->actual === false;
     }
-    
+
     function be_empty() {
         return empty($this->actual);
     }
-    
+
     function be_type($expected) {
         $expected = strtolower($expected);
         $type = strtolower(gettype($this->actual));
@@ -340,7 +340,7 @@ class Expect {
             $type === $expected,
             "expected %s to be type $expected, was $type", $this->actual);
     }
-    
+
     function have_length($expected) {
         if (is_string($this->actual))
             $length = strlen($this->actual);
@@ -351,7 +351,7 @@ class Expect {
             'expected %s to have length %d, was %d',
             $this->actual, $expected, $length);
     }
-    
+
     function have_length_within($min, $max) {
         if (is_string($this->actual))
             $length = strlen($this->actual);
@@ -362,7 +362,7 @@ class Expect {
             'expected %s to have length within %d and %d, was %d',
             $this->actual, $min, $max, $length);
     }
-    
+
     function throw_error($className=null, $message=null) {
         try {
             $func = $this->actual;
@@ -408,6 +408,7 @@ class Formatter {
         'cyan'    => 36,
         'white'   => 37
     );
+
     function color($string, $color) {
         return sprintf("\033[%dm%s\033[0m", self::$colors[$color], $string);
     }
@@ -415,28 +416,28 @@ class Formatter {
     function before() {
         $this->startTime = microtime(true);
     }
-    
+
     function beforeSuite($suite) {
         if ($suite instanceof Runner)
             return;
         if (!empty($suite->specs))
             echo $this->color("\n{$suite->description}\n", 'bold');
     }
-    
+
     function beforeSpec($spec) {
         echo "- {$spec->description}: ";
     }
-    
+
     function afterSpec($spec) {
         if ($spec->passed())
             echo $this->color("pass\n", 'green');
         else
-            echo $this->color("fail   \n", 'red');
+            echo $this->color("fail\n", 'red');
     }
-    
+
     function afterSuite($suite) {
     }
-    
+
     function after() {
         $passed = $failed = 0;
         foreach (runner()->specs as $spec) {
@@ -455,6 +456,10 @@ class Formatter {
         }
         $this->endTime = microtime(true);
         $this->runTime = $this->endTime - $this->startTime;
+        $this->banner($passed, $failed);
+    }
+
+    function banner($passed, $failed) {
         echo "\nFinished in ".number_format($this->runTime, 4)." seconds\n\n";
         echo $this->color('Passed: ', 'bold');
         echo $this->color($passed, 'green');
@@ -464,51 +469,40 @@ class Formatter {
     }
 }
 
-class HtmlFormatter extends Formatter
-{
-  static $colors = array(
-    'black' => '#000000',
-    'white' => '#ffffff',
-    'red'   => '#ff2222',
-    'green' => '#33ee33',
-    'blue'  => '#0000ff'
-  );
-  
-  function color($string, $color)
-  {
-    if($color == 'bold')
-      $style = "font-weight: bold;";
-    else 
-      $style = "color:". self::$colors[$color];
-    $ret = "<span style=\"{$style}\">{$string}</span>";
-    $ret = nl2br($ret);
-    return $ret;
-  }
+class HtmlFormatter extends Formatter {
+    function color($string, $color) {
+        return "<span class=\"pecs_{$color}\">{$string}</span>";
+    }
 
-   function after() {
-      $passed = $failed = 0;
-      foreach (runner()->specs as $spec) {
-          if ($spec->failed()) {
-              $count = count($spec->failures);
-              $failed += $count;
-              $passed += $spec->assertions - $count;
-              foreach ($spec->failures as $failure) {
-                  echo nl2br("\nFAILURE:\n");
-                  echo nl2br($failure->getMessage()."\n");
-                  echo nl2br($failure->getTraceAsString()."\n");
-              }
-          }
-          else
-              $passed += $spec->assertions;
-      }
-      $this->endTime = microtime(true);
-      $this->runTime = $this->endTime - $this->startTime;
-      echo "\nFinished in ".number_format($this->runTime, 4)." seconds\n\n";
-      echo $this->color('Passed: ', 'bold');
-      echo $this->color($passed, 'green');
-      echo $this->color(' Failed: ', 'bold');
-      echo $this->color($failed, 'red');
-      echo "\n\n";
-  }
-  
+    function before() {
+        ob_start();
+        parent::before();
+    }
+
+    function after() {
+        parent::after();
+        $output = ob_get_clean();
+        echo implode("\n", array(
+            '<!DOCTYPE html>', '<html>', '<head>',
+            '<meta charset="UTF-8"/>',
+            '<style>',
+            'body { font: 14px/1.5 Helvetica, Arial; margin: 6em 1em 1em; }',
+            '#banner { position: absolute; top: 0; }',
+            '.pecs_black { color: #000; }',
+            '.pecs_bold { font-weight: bold; }',
+            '.pecs_red { color: #c33; }',
+            '.pecs_green { color: #3c3; }',
+            '</style>',
+            '</head>',
+            '<body>'
+        ));
+        echo nl2br($output, true) . "\n";
+        echo "</body>\n</html>\n";
+    }
+
+    function banner($passed, $failed) {
+        echo '<div id="banner">';
+        parent::banner($passed, $failed);
+        echo '</div>';
+    }
 }
